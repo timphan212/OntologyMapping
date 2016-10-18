abfe367756af13ed73d24967b4e24ce6a9fee8a9
Timothy Phan <timphan212@gmail.com>
2016-10-18 09:44:00 -0700

    ontology mapping setup

---
+++ OntologyMapping/src/ontologymapping/OntologyMapping.java
@@ -0,0 +1,12 @@
+package ontologymapping;
+
+public class OntologyMapping {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
