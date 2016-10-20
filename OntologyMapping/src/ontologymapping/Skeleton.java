22f7f82fb6b887eebbe229888e14547758bece26
Jay Patel <pateljay143@gmail.com>
2016-10-20 15:17:42 -0700

    Changed rdf file type

--- OntologyMapping/src/ontologymapping/Skeleton.java
+++ OntologyMapping/src/ontologymapping/Skeleton.java
@@ -1,30 +1,5 @@
 package ontologymapping;
 
-/*
- * $Id: Skeleton.java 1404 2010-03-31 08:53:09Z euzenat $
- *
- * Copyright (C) INRIA, 2006-2008, 2010
- *
- * Modifications to the initial code base are copyright of their
- * respective authors, or their employers as appropriate.  Authorship
- * of the modifications may be determined from the ChangeLog placed at
- * the end of this file.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
- * USA.
- */
 // Alignment API classes
 import org.semanticweb.owl.align.Alignment;
 import org.semanticweb.owl.align.AlignmentProcess;
@@ -36,12 +11,12 @@ import fr.inrialpes.exmo.align.impl.method.EditDistNameAlignment;
 import fr.inrialpes.exmo.align.impl.method.NameAndPropertyAlignment;
 import fr.inrialpes.exmo.align.impl.method.StringDistAlignment;
 import fr.inrialpes.exmo.align.impl.renderer.HTMLRendererVisitor;
-import java.io.FileNotFoundException;
 
 // Java standard classes
 import java.io.PrintWriter;
 import java.net.URI;
 import java.util.Properties;
+import java.io.FileNotFoundException;
 import org.semanticweb.owl.align.AlignmentException;
 
 /**
@@ -91,7 +66,7 @@ public class Skeleton {
                 } else if (outputFormat == OutputFormat.CSV) {
                     fileName = fileName + matchingTechnique + ".csv";
                 } else if (outputFormat == OutputFormat.RDF) {
-                    fileName = fileName + matchingTechnique + ".txt";
+                    fileName = fileName + matchingTechnique + ".rdf";
                 }
 
                 try ( // write match results
